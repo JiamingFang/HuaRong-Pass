834c6745b069901262641d9cb57135ab9a6095e6
unknown <1057255244@qq.com>
2019-05-28 22:26:50 -0400

    A* search works

--- Astar.py
+++ Astar.py
@@ -1,14 +1,25 @@
-from queue import PriorityQueue as PriorityQ
+from queue import PriorityQueue as PQ
+import copy
 
-class state:
+class State:
     def __init__(self,T,parent,cost):
-        self.T = T
+        self.T = copy.deepcopy(T)
         self.parent = parent
         self.cost = cost+1
+    
+    def __lt__(self, other):
+        return self.cost < other.cost
 
-        
+    def toString(self):
+        s = ""
+        for i in range(5):
+            for j in range(4):
+                s+=str(self.T[i][j])
+        return s
+
+     
 def read_puzzle(id):
-    puzzle = state([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],-1)
+    init = State([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],-1)
 
     f=None
     if id == 1:
@@ -25,28 +36,46 @@ def read_puzzle(id):
         for c in line:
             if c!="\n":
                 #print("line")
-                puzzle.T[i][j] = c
+                init.T[i][j] = int(c)
                 j+=1
         i+=1
         #print("A")
     f.close()
+    return init
 
-    print(puzzle.T)
-    print(puzzle.parent)
-    print(puzzle.cost)
+    # print(puzzle.T)
+    # print(puzzle.parent)
+    # print(puzzle.cost)
 
 
-def is__goal(state):
-    if state.T[4][1] == 1 and  state.T[4][2] == 1 and state.T[3][1] == 1 and state.T[3][2] == 1:
-        return True
-    else:
-        return False
+def is_goal(state):
+    return state.T[4][1] == 1 and  state.T[4][2] == 1 and state.T[3][1] == 1 and state.T[3][2] == 1
+
+
+def get_cost(state):
+    return state.cost
+
+
+def get_heuristic(state):
+    topLeft = None
+    for i in range(5):
+        flag = False
+        for j in range(4):
+            if state.T[i][j] == 1:
+                topLeft = [i,j]
+                flag = True
+                break
+        if flag:
+            break
+    ans = abs(3-topLeft[0]) + abs(1-topLeft[1])
+    return ans
+
 
 
 def get_successors(state):
     zero1 = None
     zero2 = None
-    successors = []
+    successors = set()
     for i in range(5):
         for j in range(4):
             if state.T[i][j] == 0:
@@ -54,62 +83,249 @@ def get_successors(state):
                     zero1 = [i,j]
                 else:
                     zero2 = [i,j]
+
+    # print(zero1)
+    # print(zero2)
     #check if there are single pieces around both spaces that can be moved into the space
-    #first move a piece down
-    if zero1[0] != 0 and state.T[zero1[0]-1,zero1[1]] == 7:
-        puzzle = state(state.T,state.T,state.cost)
-        puzzle.T[zero1[0],zero1[1]] = 7
-        puzzle.T[zero1[0]-1,zero1[1]] = 0
-        successors.extend(puzzle)
+    #move a piece right
+    if zero1[1] != 0 and state.T[zero1[0]][zero1[1]-1] == 7:
+        puzzle = State(state.T,state,state.cost)
+        puzzle.T[zero1[0]][zero1[1]] = 7
+        puzzle.T[zero1[0]][zero1[1]-1] = 0
+        successors.add(puzzle)
+        # print("right")
+        # print(state.T)
 
-    if zero2[0] != 0 and state.T[zero2[0]-1,zero2[1]] == 7:
-        puzzle = state(state.T,state.T,state.cost)
-        puzzle.T[zero2[0],zero2[1]] = 7
-        puzzle.T[zero2[0]-1,zero2[1]] = 0
-        successors.extend(puzzle)
-
-    #move a piece up
-    if zero1[0] != 4 and state.T[zero1[0]+1,zero1[1]] == 7:
-        puzzle = state(state.T,state.T,state.cost)
-        puzzle.T[zero1[0],zero1[1]] = 7
-        puzzle.T[zero1[0]+1,zero1[1]] = 0
-        successors.extend(puzzle)
-
-    if zero2[0] != 4 and state.T[zero2[0]+1,zero2[1]] == 7:
-        puzzle = state(state.T,state.T,state.cost)
-        puzzle.T[zero2[0],zero2[1]] = 7
-        puzzle.T[zero2[0]+1,zero2[1]] = 0
-        successors.extend(puzzle)
+    if zero2[1] != 0 and state.T[zero2[0]][zero2[1]-1] == 7:
+        puzzle = State(state.T,state,state.cost)
+        puzzle.T[zero2[0]][zero2[1]] = 7
+        puzzle.T[zero2[0]][zero2[1]-1] = 0
+        successors.add(puzzle)
+        # print("right2")
 
     #move a piece left
-    if zero1[1] != 3 and state.T[zero1[0],zero1[1]+1] == 7:
-        puzzle = state(state.T,state.T,state.cost)
-        puzzle.T[zero1[0],zero1[1]] = 7
-        puzzle.T[zero1[0],zero1[1]+1] = 0
-        successors.extend(puzzle)
+    if zero1[1] != 3 and state.T[zero1[0]][zero1[1]+1] == 7:
+        puzzle = State(state.T,state,state.cost)
+        puzzle.T[zero1[0]][zero1[1]] = 7
+        puzzle.T[zero1[0]][zero1[1]+1] = 0
+        successors.add(puzzle)
 
-    if zero2[1] != 3 and state.T[zero2[0],zero2[1]+1] == 7:
-        puzzle = state(state.T,state.T,state.cost)
-        puzzle.T[zero2[0],zero2[1]] = 7
-        puzzle.T[zero2[0],zero2[1]+1] = 0
-        successors.extend(puzzle)
+    if zero2[1] != 3 and state.T[zero2[0]][zero2[1]+1] == 7:
+        puzzle = State(state.T,state,state.cost)
+        puzzle.T[zero2[0]][zero2[1]] = 7
+        puzzle.T[zero2[0]][zero2[1]+1] = 0
+        successors.add(puzzle)
 
-    #move a piece right
-    if zero1[1] != 0 and state.T[zero1[0],zero1[1]-1] == 7:
-        puzzle = state(state.T,state.T,state.cost)
-        puzzle.T[zero1[0],zero1[1]] = 7
-        puzzle.T[zero1[0],zero1[1]-1] = 0
-        successors.extend(puzzle)
+    #move a piece up
+    if zero1[0] != 4 and state.T[zero1[0]+1][zero1[1]] == 7:
+        puzzle = State(state.T,state,state.cost)
+        puzzle.T[zero1[0]][zero1[1]] = 7
+        puzzle.T[zero1[0]+1][zero1[1]] = 0
+        successors.add(puzzle)
 
-    if zero2[1] != 0 and state.T[zero2[0],zero2[1]-1] == 7:
-        puzzle = state(state.T,state.T,state.cost)
-        puzzle.T[zero2[0],zero2[1]] = 7
-        puzzle.T[zero2[0],zero2[1]-1] = 0
-        successors.extend(puzzle)
+    if zero2[0] != 4 and state.T[zero2[0]+1][zero2[1]] == 7:
+        puzzle = State(state.T,state,state.cost)
+        puzzle.T[zero2[0]][zero2[1]] = 7
+        puzzle.T[zero2[0]+1][zero2[1]] = 0
+        successors.add(puzzle)
+
+    #first move a piece down
+    if zero1[0] != 0 and state.T[zero1[0]-1][zero1[1]] == 7:
+        puzzle = State(state.T,state,state.cost)
+        puzzle.T[zero1[0]][zero1[1]] = 7
+        puzzle.T[zero1[0]-1][zero1[1]] = 0
+        successors.add(puzzle)
+
+    if zero2[0] != 0 and state.T[zero2[0]-1][zero2[1]] == 7:
+        puzzle = State(state.T,state,state.cost)
+        puzzle.T[zero2[0]][zero2[1]] = 7
+        puzzle.T[zero2[0]-1][zero2[1]] = 0
+        successors.add(puzzle)
 
     #now all the single pieces are done, move the 1*2 pieces
+    #move vertical pieces right
+    if zero1[1] == zero2[1] and (zero1[0] == zero2[0]+1 or zero1[0] == zero2[0]-1) and zero1[1] != 0:
+        if (state.T[zero1[0]][zero1[1]-1] == state.T[zero2[0]][zero2[1]-1]) and state.T[zero1[0]][zero1[1]-1] != 7:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero1[0]][zero1[1]] = puzzle.T[zero1[0]][zero1[1]-1]
+            puzzle.T[zero2[0]][zero2[1]] = puzzle.T[zero2[0]][zero2[1]-1]
+            if state.T[zero1[0]][zero1[1]-1] == 1:
+                puzzle.T[zero1[0]][zero1[1]-2] = 0
+                puzzle.T[zero2[0]][zero2[1]-2] = 0
+            else:
+                puzzle.T[zero1[0]][zero1[1]-1] = 0
+                puzzle.T[zero2[0]][zero2[1]-1] = 0
+            successors.add(puzzle)
+
+    #move vertical pieces left
+    if zero1[1] == zero2[1] and (zero1[0] == zero2[0]+1 or zero1[0] == zero2[0]-1) and zero1[1] != 3:
+        if state.T[zero1[0]][zero1[1]+1] == state.T[zero2[0]][zero2[1]+1] and state.T[zero1[0]][zero1[1]+1] != 7:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero1[0]][zero1[1]] = puzzle.T[zero1[0]][zero1[1]+1]
+            puzzle.T[zero2[0]][zero2[1]] = puzzle.T[zero2[0]][zero2[1]+1]
+            if state.T[zero1[0]][zero1[1]+1] == 1:
+                puzzle.T[zero1[0]][zero1[1]+2] = 0
+                puzzle.T[zero2[0]][zero2[1]+2] = 0
+            else:
+                puzzle.T[zero1[0]][zero1[1]+1] = 0
+                puzzle.T[zero2[0]][zero2[1]+1] = 0
+            successors.add(puzzle)
+
+    #move horizontal pieces up
+    if zero1[0] == zero2[0] and (zero1[1] == zero2[1]+1 or zero1[1] == zero2[1]-1) and zero1[0] != 4:
+        if state.T[zero1[0]+1][zero1[1]] == state.T[zero2[0]+1][zero2[1]] and state.T[zero1[0]+1][zero1[1]] != 7:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero1[0]][zero1[1]] = puzzle.T[zero1[0]+1][zero1[1]]
+            puzzle.T[zero2[0]][zero2[1]] = puzzle.T[zero2[0]+1][zero2[1]]
+            if state.T[zero1[0]+1][zero1[1]] == 1:
+                puzzle.T[zero1[0]+2][zero1[1]] = 0
+                puzzle.T[zero2[0]+2][zero2[1]] = 0
+            else:
+                puzzle.T[zero1[0]+1][zero1[1]] = 0
+                puzzle.T[zero2[0]+1][zero2[1]] = 0
+            successors.add(puzzle)
+
+    #move horizontal pieces down
+    if zero1[0] == zero2[0] and (zero1[1] == zero2[1]+1 or zero1[1] == zero2[1]-1) and zero1[0] != 0:
+        if state.T[zero1[0]-1][zero1[1]] == state.T[zero2[0]-1][zero2[1]] and state.T[zero1[0]-1][zero1[1]] != 7:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero1[0]][zero1[1]] = puzzle.T[zero1[0]-1][zero1[1]]
+            puzzle.T[zero2[0]][zero2[1]] = puzzle.T[zero2[0]-1][zero2[1]]
+            if state.T[zero1[0]-1][zero1[1]] == 1:
+                puzzle.T[zero1[0]-2][zero1[1]] = 0
+                puzzle.T[zero2[0]-2][zero2[1]] = 0
+            else:
+                puzzle.T[zero1[0]-1][zero1[1]] = 0
+                puzzle.T[zero2[0]-1][zero2[1]] = 0
+            successors.add(puzzle)
+
+    #move horizontal piece right
+    if zero1[1] > 1:
+        if state.T[zero1[0]][zero1[1]-1] == state.T[zero1[0]][zero1[1]-2] and state.T[zero1[0]][zero1[1]-1] != 7 and state.T[zero1[0]][zero1[1]-1] != 1:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero1[0]][zero1[1]] = puzzle.T[zero1[0]][zero1[1]-1]
+            puzzle.T[zero1[0]][zero1[1]-2] = 0
+            successors.add(puzzle)
+
+    if zero2[1] > 1:
+        if state.T[zero2[0]][zero2[1]-1] == state.T[zero2[0]][zero2[1]-2] and state.T[zero2[0]][zero2[1]-1] != 7 and state.T[zero2[0]][zero2[1]-1] != 1:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero2[0]][zero2[1]] = puzzle.T[zero2[0]][zero2[1]-1]
+            puzzle.T[zero2[0]][zero2[1]-2] = 0
+            successors.add(puzzle)
+
+    #move horizontal piece left
+    if zero1[1] < 2:
+        if state.T[zero1[0]][zero1[1]+1] == state.T[zero1[0]][zero1[1]+2] and state.T[zero1[0]][zero1[1]+1] != 7 and state.T[zero1[0]][zero1[1]+1] != 1:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero1[0]][zero1[1]] = puzzle.T[zero1[0]][zero1[1]+1]
+            puzzle.T[zero1[0]][zero1[1]+2] = 0
+            successors.add(puzzle)
+
+    if zero2[1] < 2:
+        if state.T[zero2[0]][zero2[1]+1] == state.T[zero2[0]][zero2[1]+2] and state.T[zero2[0]][zero2[1]+1] != 7 and state.T[zero2[0]][zero2[1]+1] != 1:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero2[0]][zero2[1]] = puzzle.T[zero2[0]][zero2[1]+1]
+            puzzle.T[zero2[0]][zero2[1]+2] = 0
+            successors.add(puzzle)
+
+    #move vertical piece up
+    if zero1[0] < 3:
+        if state.T[zero1[0]+1][zero1[1]] == state.T[zero1[0]+2][zero1[1]] and state.T[zero1[0]+1][zero1[1]] != 7 and state.T[zero1[0]+1][zero1[1]] != 1:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero1[0]][zero1[1]] = puzzle.T[zero1[0]+1][zero1[1]]
+            puzzle.T[zero1[0]+2][zero1[1]] = 0
+            successors.add(puzzle)
+
+    if zero2[0] < 3:
+        if state.T[zero2[0]+1][zero2[1]] == state.T[zero2[0]+2][zero2[1]] and state.T[zero2[0]+1][zero2[1]] != 7 and state.T[zero2[0]+1][zero2[1]] != 1:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero2[0]][zero2[1]] = puzzle.T[zero2[0]+1][zero2[1]]
+            puzzle.T[zero2[0]+2][zero2[1]] = 0
+            successors.add(puzzle)
+
+    #move vertical piece down
+    if zero1[0] > 1:
+        if state.T[zero1[0]-1][zero1[1]] == state.T[zero1[0]-2][zero1[1]] and state.T[zero1[0]-1][zero1[1]] != 7 and state.T[zero1[0]-1][zero1[1]] != 1:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero1[0]][zero1[1]] = puzzle.T[zero1[0]-1][zero1[1]]
+            puzzle.T[zero1[0]-2][zero1[1]] = 0
+            successors.add(puzzle)
+
+    if zero2[0] > 1:
+        if state.T[zero2[0]-1][zero2[1]] == state.T[zero2[0]-2][zero2[1]] and state.T[zero2[0]-1][zero2[1]] != 7 and state.T[zero2[0]-1][zero2[1]] != 1:
+            puzzle = State(state.T,state,state.cost)
+            puzzle.T[zero2[0]][zero2[1]] = puzzle.T[zero2[0]-1][zero2[1]]
+            puzzle.T[zero2[0]-2][zero2[1]] = 0
+            successors.add(puzzle)
+
+    # for successor in successors:
+    #     print(successor.T)
+    return successors
 
 
+def a_star(initial_state):
+    frontier = PQ()
+    index = 0
+    frontier.put((get_heuristic(initial_state), initial_state))
+    visited = set()
+    visited.add(initial_state.toString())
+    while frontier.qsize() != 0:
+        cur = frontier.get()[1]
+        index +=1
+        # print(index)
+        # print(cur)
+        successors = get_successors(cur)
+        for s in successors:
+            if is_goal(s): 
+                print(index)
+                return s
+            priority = get_heuristic(s)+s.cost
+            # print(priority)
+            if s.toString() not in visited:
+                frontier.put((priority,s))
+                visited.add(s.toString())
+    return 
 
-read_puzzle(1)
+init = read_puzzle(1)
+print(init.T)
+ans = a_star(init)
+print(ans.cost)
+
+# test1 = State([[2,1,1,3],[2,1,1,3],[4,6,6,5],[4,7,7,5],[7,0,0,7]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],-1)
+# get_successors(test1)
+
+# test2 = State([[2, 1, 1, 3], [2, 1, 1, 3], [4, 6, 6, 5], [4, 7, 0, 5], [7, 0, 7, 7]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],2)
+# get_successors(test2)
+
+# test3 = State([[2, 1, 1, 3], [2, 1, 1, 3], [4, 6, 6, 5], [4, 0, 0, 5], [7, 7, 7, 7]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],2)
+# get_successors(test3)
+
+# test4 = State([[2, 1, 1, 3], [2, 1, 1, 3], [4, 0, 0, 5], [4, 6, 6, 5], [7, 7, 7, 7]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],2)
+# get_successors(test4)
+
+# test5 = State([[2, 1, 1, 3], [2, 1, 1, 3], [4, 0, 5, 7], [4, 0, 5, 7], [6, 6, 7, 7]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],2)
+# get_successors(test5)
+
+# test6 = State([[0, 1, 1, 3], [0, 1, 1, 3], [4, 2, 5, 7], [4, 2, 5, 7], [6, 6, 7, 7]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],2)
+# get_successors(test6)
+
+# test7 = State([[0, 1, 1, 3], [4, 1, 1, 3], [4, 2, 5, 7], [0, 2, 5, 7], [6, 6, 7, 7]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],2)
+# get_successors(test7)
+
+# test8 = State([[2, 1, 1, 3], [2, 1, 1, 3], [0, 6, 6, 0], [4,7,7,5], [4,7,7,5]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],2)
+# get_successors(test8)
+
+# test9 = State([[2, 3, 1, 1], [2, 3, 1, 1], [0, 6, 6, 0], [4,7,7,5], [4,7,7,5]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],2)
+# print(get_heuristic(test9))
+
+# test1 = State([[2,6,6,3],[2,7,4,3],[1,1,4,5],[1,1,7,5],[7,0,0,7]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],-1)
+# test2 = State([[2,4,5,3],[2,4,5,3],[6,6,7,7],[1,1,0,7],[1,1,0,7]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],-1)
+
+# get_successors(test2)
+
+# ans = a_star(test1)
+# print(ans.cost)
+
+# read_puzzle(1)
 
